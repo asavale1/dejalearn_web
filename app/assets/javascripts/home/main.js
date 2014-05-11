1e6e756b4db5ce90ba5e158b5745dfcddd8ae776
asavale1 <asavale1@yahoo.com>
2014-05-10 17:43:20 -0700

    one page scroll

---
+++ app/assets/javascripts/home/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".main").onepage_scroll({
+        	sectionContainer: "div.scroll",
+        	easing: "ease",
+        	responsiveFallback: 600,
+        	loop: true
+      	});
+});

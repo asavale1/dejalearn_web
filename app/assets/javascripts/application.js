eb658e6a4230c6c415c86b39a170ab40235ad8ea
asavale1 <asavale1@yahoo.com>
2014-05-18 12:15:06 -0700

    fix scroll error in chrome

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require_tree ./global
 //= require jquery_ujs
 //= require turbolinks

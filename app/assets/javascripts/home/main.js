2aa8686aab934fad26ca530279124b8b1f7ab44e
asavale1 <asavale1@yahoo.com>
2014-09-15 21:35:59 -0700

    update about and packaet div

--- app/assets/javascripts/home/main.js
+++ app/assets/javascripts/home/main.js
@@ -1,14 +1,75 @@
+var storyAngle = 0;
+var contactAngle = 0;
+var storyInterval;
+var contactInterval;
+
 $(document).ready(function(){
 	$(".main").onepage_scroll({
-        	sectionContainer: "div.scroll",
-        	easing: "ease",
-        	responsiveFallback: 600,
-        	loop: true,
-        	pagination: false,
-      	});
+    	sectionContainer: "div.scroll",
+    	easing: "ease",
+    	responsiveFallback: 600,
+    	loop: true,
+    	pagination: false,
+	});
+
+    $("#story-img").hover(function(){
+        console.log("In Hover Story");
+        window.clearInterval(storyInterval);
+    }, function(){
+        console.log("Out of Hover Story");
+        setStoryInterval();
+        //story
+    });
+
+    $("#contact-img").hover(function(){
+        console.log("In Hover Contact");
+        window.clearInterval(contactInterval);
+    }, function(){
+        console.log("Out of Hover Contact");
+        setContactInterval();
+    });
+
+    setStoryInterval();
+    setContactInterval();
 });
+
+function setStoryInterval(){
+    storyInterval = window.setInterval(function(){
+        if(storyAngle == 360){
+            storyAngle = 0;
+        }
+
+        rotate(storyAngle, "story-img");
+        storyAngle += 1;
+    }, 10);
+}
+
+function setContactInterval(){
+    contactInterval = window.setInterval(function(){
+        if(contactAngle == 360){
+            contactAngle = 0;
+        }
+
+        rotate(contactAngle, "contact-img");
+        contactAngle += 1;
+    }, 10);
+}
+
+function rotate(degrees, id){
+    $("#" + id).css(
+        {
+            '-webkit-transform' : 'rotate('+ degrees +'deg)',
+            '-moz-transform' : 'rotate('+ degrees +'deg) ',
+            '-ms-transform' : 'rotate('+ degrees +'deg) ',
+            'transform' : 'rotate('+ degrees +'deg) '
+        }
+    );
+}
 
 function scroll(index){
 	console.log(index);
 	$(".main").moveTo(index);
 }
+
+
+

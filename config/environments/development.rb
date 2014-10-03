9d7e2587ae9804f76ab33ae1635e8488dc691564
Ameya Savale <asavale1@yahoo.com>
2014-10-02 21:52:27 -0700

    email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,8 +21,8 @@ Dejalearn::Application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => 'gmail.com',
-    :user_name            => 'kingsavale@gmail.com',
-    :password             => 'Century21',
+    :user_name            => 'email@gmail.com',
+    :password             => 'password',
     :authentication       => 'plain',
     :enable_starttls_auto => true  
   }

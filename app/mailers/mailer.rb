384660ab863f223ff464e47e7840105d873716b1
ShantanuPhadke <shantanu.cricket@gmail.com>
2014-10-27 20:44:45 -0700

    AJAX in About

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,9 +1,10 @@
 class Mailer < ActionMailer::Base
   default from: "notify@dejalearn.com"
 
-  def notify_email(name, email)
+  def notify_email(name, email,content)
   	@name = name
   	@email = email
+  	@Content = content
   	mail(to: 'asavale1@yahoo.com', subject: 'Notify about app')
   end
 

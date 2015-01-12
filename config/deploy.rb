57e50e454f4cc692211bb25513d510b2f6d63c71
Ameya <asavale1@yahoo.com>
2015-01-11 17:22:01 -0800

    link recaptch file

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :use_sudo, false
 
 set :tmp_dir, "/home/asavale1/tmp"
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/initializers/recaptcha.rb}
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 

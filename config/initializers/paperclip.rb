5431110bf64f8a0beb35ffe1d2e6ace6fbc49917
Ameya <asavale1@yahoo.com>
2015-01-29 20:06:47 -0800

    xml

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = "/packet/:attachment/:id_partition/:style/#{rand(100**10)}.:filename"
+Paperclip::Attachment.default_options[:path] = "/packet/:attachment/:id_partition/:style/#{rand(10**10)}.:filename"

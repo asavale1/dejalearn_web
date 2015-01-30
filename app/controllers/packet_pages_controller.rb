8a4f02ae2136a1b1285c34ac22984c37993e44c9
Ameya <asavale1@yahoo.com>
2015-01-29 19:38:17 -0800

    xml

--- app/controllers/packet_pages_controller.rb
+++ app/controllers/packet_pages_controller.rb
@@ -120,7 +120,7 @@ class PacketPagesController < ApplicationController
 				:id => packet.id,
 				:packet_title => packet.title,
 				:description => packet.description,
-				:url => packet.xml,
+				:url => PcketPacgesHelper.get_alt_url(packet.xml),
 				:ex_count => packet.count
 			}
 		end

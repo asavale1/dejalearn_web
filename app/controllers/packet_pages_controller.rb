610f331aa7cf6804567ad9b60d26f11d5040eaac
Ameya <asavale1@yahoo.com>
2015-01-31 21:29:48 -0800

    pp

--- app/controllers/packet_pages_controller.rb
+++ app/controllers/packet_pages_controller.rb
@@ -79,6 +79,7 @@ class PacketPagesController < ApplicationController
 
 		PacketPagesHelper.save_images(params, packet.id)		
 		packet.xml = PacketPagesHelper.create_xml(params, packet.id)
+		puts "\n\nPACKET XML\t#{packet.xml}\n\n"
 		packet.created_by = current_user.id
 		packet.save
 		

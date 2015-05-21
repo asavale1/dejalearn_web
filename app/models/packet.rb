22723107ab94dfc9ed1b6f9be1f71bfefe87b047
Ameya <asavale1@yahoo.com>
2015-05-20 18:54:27 -0700

    remove relation to images

--- app/models/packet.rb
+++ app/models/packet.rb
@@ -12,7 +12,7 @@ class Packet < ActiveRecord::Base
 	has_many :tag_packets
 	has_many :tags, :through => :tag_packets
 
-	has_many :images, dependent: :destroy
+	#has_many :images, dependent: :destroy
 
 	has_attached_file :xml
 	validates_attachment_content_type :xml, :content_type => ["text/xml", "application/xml"] 

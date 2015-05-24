b9751dc6b0925c4aba037b883ffd2bd2eaa48476
Ameya <asavale1@yahoo.com>
2015-05-24 12:34:44 -0700

    remove tag_packets when tag destroyed

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-	has_many :tag_packets
+	has_many :tag_packets, dependent: :destroy
 	has_many :packets, :through => :tag_packets
 
 	validates :tag, uniqueness: { case_sensitive: false }
